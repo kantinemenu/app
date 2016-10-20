e624fb5255c8766b23f4a126e58b95ca482adc39
Troels Ugilt Jensen <tujensen@gmail.com>
2016-10-20 12:39:35 +0200

    Added star. Added deferred to get of menu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('MenuCtrl', function($scope, Menu, $rootScope) {
+.controller('MenuCtrl', function($scope, Menu, $filter) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -9,28 +9,51 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.menu = Menu.all();
+  Menu.all().then(
+    function (data) {
+      $scope.menu = data;
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
+
+  $scope.star = function (menuItem) {
+    menuItem.star = !menuItem.star;
+
+    if (menuItem.star) {
+      console.log('Schedule til: ', $filter('date')((new Date(menuItem.date)).getTime() - 5 * 60 * 60 * 1000, 'medium'));
+
+      cordova.plugins.notification.local.schedule({
+        id: 1,
+        title: menuItem.name,
+        text: menuItem.details,
+        at: (new Date(menuItem.date)).getTime() - 5 * 60 * 60 * 1000,
+        data: menuItem.date
+      });
+    }
+    else {
+      console.log('cancel 1');
+
+      cordova.plugins.notification.local.cancel(1);
+    }
+  };
 })
 
 .controller('PromptCtrl', function($scope) {
 })
 
 .controller('MenuDetailCtrl', function($scope, $stateParams, Menu) {
-  console.log($stateParams);
-  $scope.menuItem = Menu.get($stateParams.menuId);
+  Menu.get($stateParams.menuId).then(
+    function (data) {
+      $scope.menuItem = data;
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
 })
 
 .controller('SettingsCtrl', function($scope) {
-  $scope.settings = {
-  };
-  var now = new Date().getTime();
-  var _5_SecondsFromNow = new Date(now + 5 * 1000);
 
-  cordova.plugins.notification.local.schedule({
-    id: 1,
-    title: 'Super',
-    text: 'Duper',
-    at: _5_SecondsFromNow,
-    data: '2016-10-19'
-  });
 });
